d7eca23b6eb4a6ff8f79299c818f7aa31d521279
Fazrul <mfazrul07@gmail.com>
2022-04-10 16:58:31 +0800

    make it better

--- homestay-api/app/Providers/AppServiceProvider.php
+++ homestay-api/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        return Schema::defaultStringLength(191);
     }
 }
